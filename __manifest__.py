1d99b8810a5f9ba1c8ce6d04496086c3c65522b6
joseCastroB <josecastrobarreto10@gmail.com>
2025-12-24 09:56:17 -0500

    Se agrega el modulo de contactos

---
+++ __manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Personalización de Contactos NAM',
+    'version': '1.0',
+    'category': 'Tools',
+    'summary': 'Campos bancarios extras en ficha de contacto',
+    'author': 'Jose Castro',
+    'depends': ['base', 'account', 'purchase'],
+    'data': [
+        'views/res_partner_view.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
---
+++ models/res_partner.py
@@ -0,0 +1,11 @@
+from odoo import models, fields 
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    nam_entidad_bancaria = fields.Char(string='Entidad Bancaria')
+    nam_cta_cte = fields.Char(string='CTA. CTE. (S/.)')
+    nam_cci = fields.Char(string='CCI (S/.)')
+    nam_cta_cte_usd = fields.Char(string='CTA. CTE. (USD)')
+    nam_cci_usd = fields.Char(string='CCI (USD)')
+    nam_cta_detracciones = fields.Char(string='Cta. Detracciones')
